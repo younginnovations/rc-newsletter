bc69308fa5ed8c7503509c0851154f6c187449d7
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-12-12 13:10:09 +0545

    send:email time changed to daily()

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('send:email')->everyMinute();
+        $schedule->command('send:email')->daily();
     }
 
     /**
