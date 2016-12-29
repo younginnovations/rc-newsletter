fe884e9b8e39fba9e06b872e8a9032e03eb1c894
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-12-29 15:14:34 +0545

    formatting in mail.php

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => 'noreply@resourcecontracts.com',
-        'name' => 'Subsite Newsletter',
+        'name'    => 'Subsite Newsletter',
     ],
 
     /*
