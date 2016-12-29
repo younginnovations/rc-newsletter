90b3b3978bfaa15889add31779b5751b3ba17084
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-12-29 14:03:33 +0545

    some css added

--- resources/views/admin/page/settings.blade.php
+++ resources/views/admin/page/settings.blade.php
@@ -1,7 +1,7 @@
 @include('partials/head-admin')
 
 <h3 class="content__title"> Settings</h3>
-<section id="subscribed-user-list">
+<section id="settings-container">
 	<form class="form form--subscription" role="form" action="{{route('admin.settings.post')}}" method="post">
 		<div class="form__body">
 			@if(Session::has('success'))
